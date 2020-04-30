8b35cf5e70af8886049235518e84b62a0e82fc61
sachinrr147 <sachin.sahu@inpixon.com>
2020-04-30 14:17:23 +0530

    mongoos

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,14 +1,7 @@
 const mongoose = require("mongoose");
-try {
-  mongoose.connect(
-    "mongodb://sachin-node-application.herokuapp.com/tast_magaer_api",
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true
-    }
-  );
-} catch (e) {
-  console.log("------------" + e);
-  console.log("------------" + process);
-}
+const validator = require("validator");
+mongoose.connect("mongodb://localhost:27017/tast_magaer_api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
