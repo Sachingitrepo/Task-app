64ae4cf45b924f04821f0742bb8bd06f0fc722b6
sachinrr147 <sachin.sahu@inpixon.com>
2020-04-30 12:55:32 +0530

    mongoos

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb://sachin-node-application.herokuapp.com/tast_magaer_api",
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  }
-);
+try {
+  mongoose.connect(
+    "mongodb://sachin-node-application.herokuapp.com/tast_magaer_api",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    }
+  );
+} catch (e) {
+  console.log("------------" + e);
+  console.log("------------" + process);
+}
